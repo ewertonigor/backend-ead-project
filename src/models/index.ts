1877f6ebc0e881727633e8df720bc571ad5a98d4
Ewerton Igor <ewertonigor@icloud.com>
2024-02-25 19:07:37 -0300

    Tablea de episódios

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,13 +1,20 @@
 import { Category } from "./Category";
 import { Course } from "./Course";
+import { Episode } from "./Episode";
 
 // Uma categoria vai ter muitas cursos
 Category.hasMany(Course)
 
 // Curso pertence apenas a uma categoria.
+// Curso tem muitos episodes
 Course.belongsTo(Category)
+Course.hasMany(Episode)
+
+// Episodio está ligado a curso
+Episode.belongsTo(Course)
 
 export {
   Category,
-  Course
+  Course,
+  Episode
 }
