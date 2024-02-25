b0a4bb79be42403905a9baf1bb290af5748210cd
Ewerton Igor <ewertonigor@icloud.com>
2024-02-25 12:48:35 -0300

    Relacionamentos entre modelos

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,5 +1,13 @@
 import { Category } from "./Category";
+import { Course } from "./Course";
+
+// Uma categoria vai ter muitas cursos
+Category.hasMany(Course)
+
+// Curso pertence apenas a uma categoria.
+Course.belongsTo(Category)
 
 export {
-  Category
+  Category,
+  Course
 }
