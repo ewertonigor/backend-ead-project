67901028f974e768fd429757b4fc99adf0089593
Ewerton Igor <ewertonigor@icloud.com>
2024-02-25 22:34:27 -0300

    Upload de arquivos no AdminJS

--- src/adminjs/resources/episode.ts
+++ src/adminjs/resources/episode.ts
@@ -1,4 +1,6 @@
-import { ResourceOptions } from "adminjs";
+import uploadFileFeature from "@adminjs/upload";
+import { FeatureType, ResourceOptions } from "adminjs";
+import path from "path";
 
 export const episodeResourceOptions: ResourceOptions = {
   navigation: 'Catálogo',
@@ -7,3 +9,19 @@ export const episodeResourceOptions: ResourceOptions = {
   listProperties: ['id', 'name', 'courseId', 'order', 'secondsLong'],
   showProperties: ['id', 'name', 'synopsis', 'courseId', 'order', 'videoUrl', 'secondsLong', 'createdAt', 'updatedAt']
 }
+
+export const episodeResourceFeatures: FeatureType[] = [
+  uploadFileFeature({
+    provider: {
+      local: {
+        bucket: path.join(__dirname, '..', '..', '..', 'uploads')
+      }
+    },
+    properties: {
+      key: 'videoUrl',
+      file: 'uploadVideo'
+    },
+    // Todos os videos do mesmo curso irão ficar na mesma pasta
+    uploadPath: (record, filename) => `videos/course-${record.get('courseId')}/${filename}`
+  })
+]
