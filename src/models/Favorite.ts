87e09b5c9f0646b10a97a1dde0fa22e8bbf3d327
Ewerton Igor <ewertonigor@icloud.com>
2024-03-02 12:15:19 -0300

    Obtendo os cursos favoritos

--- src/models/Favorite.ts
+++ src/models/Favorite.ts
@@ -9,8 +9,8 @@ export interface Favorite {
 }
 
 export interface FavoriteIntance extends Model<Favorite>, Favorite {
-  course?: CourseInstance,
-  user?: UserInstance
+  Course?: CourseInstance,
+  User?: UserInstance
 }
 
 export const Favorite = sequelize.define<FavoriteIntance, Favorite>('Favorite', {
